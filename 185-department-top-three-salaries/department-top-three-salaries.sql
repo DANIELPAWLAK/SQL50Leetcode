e800288235751ddcbfd494d77afa0b1a1476edee
DANIELPAWLAK <66480754+DANIELPAWLAK@users.noreply.github.com>
2024-08-10 20:09:33 +0100

    Time: 1315 ms (18.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,15 @@
+select
+    d.name as department,
+    e.name as employee,
+    e.salary as salary
+from
+    employee e
+    join department d on e.departmentid = d.id
+where
+    (
+        select count(distinct salary)
+        from employee e2
+        where e2.departmentid = e.departmentid and e2.salary >= e.salary
+    ) <= 3
+order by
+    department, salary desc;
