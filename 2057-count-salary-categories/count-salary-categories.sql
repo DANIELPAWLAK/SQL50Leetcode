6fad9bdc0e6b86cefff1eb8ed14296c43b9a82b4
DANIELPAWLAK <66480754+DANIELPAWLAK@users.noreply.github.com>
2024-08-10 19:34:41 +0100

    Time: 1565 ms (69.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,18 @@
+select 
+    'Low Salary' as category, 
+    count(case when income < 20000 then 1 end) as accounts_count
+from Accounts
+
+union 
+
+select 
+    'Average Salary' as category, 
+    count(case when income between 20000 and 50000 then 1 end) as accounts_count
+from Accounts
+
+union 
+
+select 
+    'High Salary' as category, 
+    count(case when income > 50000 then 1 end) as accounts_count
+from Accounts;
