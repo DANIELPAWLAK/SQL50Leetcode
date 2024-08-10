679d0bdb76afe436db6effb45a5570fa892be1a2
DANIELPAWLAK <66480754+DANIELPAWLAK@users.noreply.github.com>
2024-08-10 14:25:05 +0100

    Time: 1296 ms (40.70%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,1 +1,1 @@
-select Product.product_name, Sales.year, Sales.price from Sales natural left join Product
+select product_name, year, price from Sales join Product on Sales.product_id = Product.product_id;
